74c86db483941929271ee61aea465424578bd4b5
Nauzer <mark.naus@gmail.com>
2017-12-12 11:30:15 +0100

    NEW - use ES6

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-
-module.exports = {
-    Geofence: require('./lib/geofence'),
-    GeofencedGroup: require('./lib/geofenced_group'),
-    GeofenceCache: require('./lib/geofence_cache.js')
-};
+export * from './lib/geofence';
+export * from './lib/geofenced-group';
+export * from './lib/utils';
